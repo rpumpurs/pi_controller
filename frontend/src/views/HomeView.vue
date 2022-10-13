d19d9b1863cde8d3dc437bd63e0706935832bfe9
Rudolfs Pumpurs <rudolfs.pumpurs@gmail.com>
2022-10-13 13:42:23 +0300

    Move sink list top

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <template>
-  <DeviceList/>
-  <SinkList/>
+  <div class="mb-3">
+    <SinkList/>
+  </div>
+  <div class="mb-3">
+    <DeviceList/>
+  </div>
 </template>
 
 <script>
-import DeviceList from "@/components/BluetoothDeviceList.vue";
 import SinkList from "@/components/SinkList.vue";
+import DeviceList from "@/components/BluetoothDeviceList.vue";
 export default {
   name: "index",
   components: {SinkList, DeviceList},
